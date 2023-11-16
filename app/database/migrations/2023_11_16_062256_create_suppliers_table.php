b9bb82afa49706102e3b4d48daffec3a807bfe21
TranNghia281203 <146937143+TranNghia281203@users.noreply.github.com>
2023-11-16 13:26:47 +0700

    implement model migration suppliers

--- app/database/migrations/2023_11_16_062256_create_suppliers_table.php
+++ app/database/migrations/2023_11_16_062256_create_suppliers_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('addresss');
+            $table->string('phone');
+            $table->string('description');
+            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('suppliers');
     }
 };
