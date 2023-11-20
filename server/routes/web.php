eaf66e72ef7cc2452dbb0626236a7c68cb7fa963
TranThanhTuan <0306211208@caothang.edu.vn>
2023-11-20 14:24:48 +0700

    reup project

--- server/routes/web.php
+++ server/routes/web.php
@@ -8,6 +8,7 @@ Route::post('/admin/login-handler', [AdminsController::class, 'loginHandler'])->
 // admin - viet thanh
 Route::middleware('auth')->group(function(){
     Route::prefix('admin')->group(function(){
+        //Viet thanh
         Route::name('admin.')->group(function(){
             Route::get('logout', [AdminsController::class, 'logout'])->name('logout');
             Route::get('/',[AdminsController::class,'index'])->name('index'); 
@@ -20,15 +21,21 @@ Route::middleware('auth')->group(function(){
             Route::get('untrash/{id}', [AdminsController::class, 'untrash'])->name('untrash');
         });
     });
+    //Thanh tuan
+    Route::prefix('category')->group(function(){
+        Route::name('category.')->group(function(){
+            Route::get('/',[CategoriesController::class,'index'])->name('index');
+            Route::get('create',[CategoriesController::class,'create'])->name('create');
+            Route::post('store',[CategoriesController::class,'store'])->name('store');
+            Route::get('edit/{id}',[CategoriesController::class,'edit'])->name('edit');
+            Route::post('edit/{id}',[CategoriesController::class,'update'])->name('.update');
+            Route::post('destroy/{id}', [CategoriesController::class, 'destroy'])->name('destroy');
+            Route::get('trash', [CategoriesController::class, 'trash'])->name('trash');
+            Route::get('untrash/{id}', [CategoriesController::class, 'untrash'])->name('untrash');
+        });
+    });
 });
 
 
-Route::get('/category',[CategoriesController::class,'index'])->name('category.index');
-Route::get('/category/create',[CategoriesController::class,'create'])->name('category.create');
-Route::post('/category/store',[CategoriesController::class,'store'])->name('category.store');
-Route::get('/category/edit/{id}',[CategoriesController::class,'edit'])->name('category.edit');
-Route::post('/category/edit/{id}',[CategoriesController::class,'update'])->name('category.update');
-Route::post('/category/destroy/{id}', [CategoriesController::class, 'destroy'])->name('category.destroy');
-Route::get('/category/trash', [CategoriesController::class, 'trash'])->name('category.trash');
-Route::get('/category/untrash/{id}', [CategoriesController::class, 'untrash'])->name('category.untrash');
+
 
