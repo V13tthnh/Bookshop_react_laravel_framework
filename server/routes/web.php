b936a2c8094bd9668cae17662d0afb4fd227d293
TranNghia281203 <146937143+TranNghia281203@users.noreply.github.com>
2023-11-20 23:38:05 +0700

    feature/author --nghia

--- server/routes/web.php
+++ server/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\AdminsController;
 use  App\Http\Controllers\CategoriesController;
+use  App\Http\Controllers\AuthorController;
 Route::get('/admin/login',[AdminsController::class,'login'])->name('admin.login')->middleware('guest');
 Route::post('/admin/login-handler', [AdminsController::class, 'loginHandler'])->name('admin.loginHandler');
 // admin - viet thanh
@@ -32,6 +33,19 @@ Route::middleware('auth')->group(function(){
             Route::post('destroy/{id}', [CategoriesController::class, 'destroy'])->name('destroy');
             Route::get('trash', [CategoriesController::class, 'trash'])->name('trash');
             Route::get('untrash/{id}', [CategoriesController::class, 'untrash'])->name('untrash');
+        });
+    });
+    //Thanh Nghia
+    Route::prefix('author')->group(function(){
+        Route::name('author.')->group(function(){
+            Route::get('/',[AuthorController::class,'index'])->name('index');
+            Route::get('create',[AuthorController::class,'create'])->name('create');
+            Route::post('store',[AuthorController::class,'store'])->name('store');
+            Route::get('edit/{id}',[AuthorController::class,'edit'])->name('edit');
+            Route::post('update/{id}',[AuthorController::class,'update'])->name('update');
+            Route::post('destroy/{id}',[AuthorController::class,'destroy'])->name('destroy');
+            Route::get('trash', [AuthorController::class, 'trash'])->name('trash');
+            Route::get('untrash/{id}', [AuthorController::class, 'untrash'])->name('untrash');
         });
     });
 });
