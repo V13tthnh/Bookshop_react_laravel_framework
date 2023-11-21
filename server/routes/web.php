b6bd625702dfb63d013133b15c86cc29b8dcecb7
TranThanhTuan <0306211208@caothang.edu.vn>
2023-11-21 16:49:26 +0700

    Update web.php

--- server/routes/web.php
+++ server/routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\AdminsController;
 use  App\Http\Controllers\CategoriesController;
-use  App\Http\Controllers\SuppliersController;
+// use  App\Http\Controllers\SuppliersController;
+
 Route::get('/admin/login',[AdminsController::class,'login'])->name('admin.login')->middleware('guest');
 Route::post('/admin/login-handler', [AdminsController::class, 'loginHandler'])->name('admin.loginHandler');
 // admin - viet thanh
@@ -37,20 +38,21 @@ Route::middleware('auth')->group(function(){
             Route::get('untrash/{id}', [CategoriesController::class, 'untrash'])->name('untrash');
         });
     });
-    Route::prefix('supplier')->group(function(){
-        Route::name('supplier.')->group(function(){
-            Route::get('/',[SuppliersController::class,'index'])->name('index');
-            Route::get('create',[SuppliersController::class,'create'])->name('create');
-            Route::get('show/{id}',[AdminsController::class,'show'])->name('show'); 
-            Route::post('store',[SuppliersController::class,'store'])->name('store');
-            Route::get('edit/{id}',[SuppliersController::class,'edit'])->name('edit');
-            Route::post('update',[SuppliersController::class,'update'])->name('update');
-            Route::post('destroy/{id}', [SuppliersController::class, 'destroy'])->name('destroy');
-            Route::get('delete/{id}', [SuppliersController::class, 'delete'])->name('delete');
-            Route::get('trash', [SuppliersController::class, 'trash'])->name('trash');
-            Route::get('untrash/{id}', [SuppliersController::class, 'untrash'])->name('untrash');
-        });
-    });
+
+    // Route::prefix('supplier')->group(function(){
+    //     Route::name('supplier.')->group(function(){
+    //         Route::get('/',[SuppliersController::class,'index'])->name('index');
+    //         Route::get('create',[SuppliersController::class,'create'])->name('create');
+    //         Route::get('show/{id}',[AdminsController::class,'show'])->name('show'); 
+    //         Route::post('store',[SuppliersController::class,'store'])->name('store');
+    //         Route::get('edit/{id}',[SuppliersController::class,'edit'])->name('edit');
+    //         Route::post('update',[SuppliersController::class,'update'])->name('update');
+    //         Route::post('destroy/{id}', [SuppliersController::class, 'destroy'])->name('destroy');
+    //         Route::get('delete/{id}', [SuppliersController::class, 'delete'])->name('delete');
+    //         Route::get('trash', [SuppliersController::class, 'trash'])->name('trash');
+    //         Route::get('untrash/{id}', [SuppliersController::class, 'untrash'])->name('untrash');
+    //     });
+    // });
 });
 
 
